33bbf34948fb8c444797280f350411cde0aac6ef
Julien J Tierny <julien.tierny@gmail.com>
2024-06-07 17:17:25 +0200

    clang-formatting

--- core/base/mergeTreeClustering/MergeTreeClustering.h
+++ core/base/mergeTreeClustering/MergeTreeClustering.h
@@ -25,7 +25,6 @@
 ///   href="https://topology-tool-kit.github.io/examples/mergeTreeWAE/">Merge
 ///   Tree Wasserstein Auto-Encoder example</a> \n
 
-
 #define treesMatchingVector \
   std::vector<std::vector<std::tuple<ftm::idNode, ftm::idNode, double>>>
 #define matchingVectorType std::vector<treesMatchingVector>
@@ -275,7 +274,7 @@ namespace ttk {
 #ifdef TTK_ENABLE_OPENMP4
 #pragma omp parallel for schedule(dynamic)                     \
   shared(centroids, centroids2, oldCentroids_, oldCentroids2_) \
-    num_threads(this->threadNumber_) if(parallelize_)
+  num_threads(this->threadNumber_) if(parallelize_)
 #endif
         for(unsigned int i = 0; i < centroids.size(); ++i) {
           std::vector<std::tuple<ftm::idNode, ftm::idNode, double>> matching,
