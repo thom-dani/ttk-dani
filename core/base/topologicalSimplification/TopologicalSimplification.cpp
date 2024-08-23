6f386fdd71ca8a33ebb2756c1e213b7dccb77431
Julien J Tierny <julien.tierny@gmail.com>
2024-08-23 16:08:51 +0200

    fix refactoring

--- core/base/topologicalSimplification/TopologicalSimplification.cpp
+++ core/base/topologicalSimplification/TopologicalSimplification.cpp
@@ -3,7 +3,3 @@
 ttk::TopologicalSimplification::TopologicalSimplification() {
   this->setDebugMsgPrefix("TopologicalSimplification");
 }
-
-ttk::TopologicalOptimization::TopologicalOptimization() {
-  this->setDebugMsgPrefix("TopologicalOptimization");
-}
