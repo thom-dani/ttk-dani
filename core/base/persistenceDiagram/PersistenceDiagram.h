124a22c5fdaa8902ab8e7133bbbd2e285631d007
Julien Tierny <julien.tierny@gmail.com>
2024-09-17 07:32:53 +0200

    [doc] adding topoOpt torus links (base layer)

--- core/base/persistenceDiagram/PersistenceDiagram.h
+++ core/base/persistenceDiagram/PersistenceDiagram.h
@@ -133,6 +133,9 @@
 ///   href="https://topology-tool-kit.github.io/examples/topologicalOptimization_darkSky//">Topological
 ///   Optimization DarkSky</a>\n
 ///   - <a
+///   href="https://topology-tool-kit.github.io/examples/topologicalOptimization_torus//">Topological
+///   Optimization for Torus Repair example</a>\n
+///   - <a
 ///   href="https://topology-tool-kit.github.io/examples/tribute/">Tribute
 ///   example</a> \n
 ///   - <a
